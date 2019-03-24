581618d4b1edc443f4b838735416b06c66ca3e04
Shiny <sshn@pm.me>
2019-03-24 23:19:55 +0100

    TP4 ?

--- menu.php
+++
@@ -1,5 +0,0 @@
-<ul>
-    <li><a href="./index.php?page=home">Accueil</a></li>
-    <li><a href="./index.php?page=list">Liste</a></li>
-    <li><a href="./index.php?page=new">Nouveau</a></li>
-</ul>
